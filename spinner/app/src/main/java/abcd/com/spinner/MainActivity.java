72e1bcb38e4a36027bb66eddc4d78ef2adaa615d
snehapriyaa <devi.mathi@gmail.com>
2016-04-12 23:48:19 +0530

    commit

--- spinner/app/src/main/java/abcd/com/spinner/MainActivity.java
+++ spinner/app/src/main/java/abcd/com/spinner/MainActivity.java
@@ -1,43 +1,262 @@
 package abcd.com.spinner;
 
-import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 
-public class MainActivity extends Activity {
-    
+public class MainActivity extends AppCompatActivity {
+    static final int SocketServerPORT = 8080;
+
+    TextView infoIp, infoPort, News;
+
+    String Log = "";
+
+    List<mClient> userList;
+    ServerSocket serverSocket;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        infoIp = (TextView) findViewById(R.id.infoip);
+        infoPort = (TextView) findViewById(R.id.infoport);
+        News = (TextView) findViewById(R.id.news);
+
+        infoIp.setText(getIpAddress());
+
+        userList = new ArrayList<mClient>();
+
+        ServerThread mchatServerThread = new ServerThread();
+        mchatServerThread.start();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class ServerThread extends Thread {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public void run() {
+            Socket socket = null;
+
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        infoPort.setText(serverSocket.getLocalPort());
+                    }
+                });
+
+                while (true) {
+                    socket = serverSocket.accept();
+                    mClient client = new mClient();
+                    userList.add(client);
+                    ConnectThread connectThread = new ConnectThread(client, socket);
+                    connectThread.start();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    private class ConnectThread extends Thread {
+
+        Socket socket;
+        mClient connectClient;
+        String newsToSend = "";
+
+        ConnectThread(mClient client, Socket socket){
+            connectClient = client;
+            this.socket= socket;
+            client.socket = socket;
+            client.mThread = this;
+        }
+
+        @Override
+        public void run() {
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+
+            try {
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                String n = dataInputStream.readUTF();
+
+                connectClient.name = n;
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                    }
+                });
+
+
+                while (true) {
+                    if (dataInputStream.available() > 0) {
+                        String newMsg = dataInputStream.readUTF();
+
+
+                        Log += n + ": " + newMsg;
+                        MainActivity.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                News.setText(Log);
+                            }
+                        });
+
+                        broadcastMsg(n + ": " + newMsg);
+                    }
+
+                    if(!newsToSend.equals("")){
+                        dataOutputStream.writeUTF(newsToSend);
+                        dataOutputStream.flush();
+                        newsToSend = "";
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        }
+
+        private void sendNews(String msg){
+            newsToSend = msg;
+        }
+
+    }
+
+    private void broadcastMsg(String msg){
+        for(int i=0; i<userList.size(); i++){
+            userList.get(i).mThread.sendNews(msg);
+            Log +=  userList.get(i).name + "\n";
+            Notify();
+        }
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                News.setText(Log);
+            }
+        });
+    }
+
+    private void Notify() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+
+        Notification notification = new Notification(R.drawable.notification_template_icon_bg,"you got new post", System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this,NotificationView.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        notification.setLatestEventInfo(MainActivity.this,"spinner","you got new post", pendingIntent);
+        notificationManager.notify(9999, notification);
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "SiteLocalAddress: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+    class mClient {
+        String name;
+        Socket socket;
+        ConnectThread mThread;
+
+    }
+
 }
---
+++ spinner/app/src/main/java/abcd/com/spinner/NotificationView.java
@@ -0,0 +1,15 @@
+package abcd.com.spinner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by snehapriyaa on 4/12/2016.
+ */
+public class NotificationView extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
